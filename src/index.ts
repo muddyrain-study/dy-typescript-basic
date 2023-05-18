e28a4d4228874052ead1a10ebef64919093b89de
“muddyrain” <qiu3291002845@gmail.com>
2023-05-18 17:04:39 +0800

    feat: 装饰器

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,13 @@
 import 'reflect-metadata';
-import axios from 'axios';
-import { Type, plainToClass } from 'class-transformer';
 
 class User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  @Type(() => Number)
+  @Reflect.metadata('a', 'b')
+  login: string;
+
+  @Reflect.metadata('a', 'b')
   age: number;
-  getName() {
-    return this.firstName + ' ' + this.lastName;
-  }
-  isAdult() {
-    return this.age > 36 && this.age < 60;
+
+  save() {
+    console.log('save');
   }
 }
-
-axios
-  .get(
-    'https://api.myjson.online/v1/records/a646f3ff-e474-4e19-b641-e56fd3b47394'
-  )
-  .then(resp => resp.data.data)
-  .then(users => {
-    const us = plainToClass(User, users) as any as User[];
-    for (const u of us) {
-      console.log(typeof u.age, u.age);
-    }
-  });
