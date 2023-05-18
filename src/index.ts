762ef45ef94cc5d2d89c9f1a72b055d31a9ab678
“muddyrain” <qiu3291002845@gmail.com>
2023-05-18 16:21:26 +0800

    feat: class-transformer  class-validator

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
-import { printObj, descriptor } from './Descriptor';
+import 'reflect-metadata';
+import axios from 'axios';
+import { Type, plainToClass } from 'class-transformer';
 
-@descriptor('文章')
-class Article {
-  @descriptor('标题')
-  title: string;
-
-  @descriptor('内容')
-  content: string;
-
-  @descriptor('日期')
-  date: Date;
+class User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  @Type(() => Number)
+  age: number;
+  getName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+  isAdult() {
+    return this.age > 36 && this.age < 60;
+  }
 }
 
-const ar = new Article();
-ar.title = 'xxxx';
-ar.content = '嘻嘻嘻';
-ar.date = new Date();
-
-printObj(ar);
+axios
+  .get(
+    'https://api.myjson.online/v1/records/a646f3ff-e474-4e19-b641-e56fd3b47394'
+  )
+  .then(resp => resp.data.data)
+  .then(users => {
+    const us = plainToClass(User, users) as any as User[];
+    for (const u of us) {
+      console.log(typeof u.age, u.age);
+    }
+  });
