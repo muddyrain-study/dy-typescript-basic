61d27c920d548f56b26a7ccd714aef60d812f630
“muddyrain” <qiu3291002845@gmail.com>
2023-05-19 09:06:36 +0800

    feat: 类型演算

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,48 @@
 interface User {
-  loginId: string;
-  loginWd: string;
   age: number;
+  name: string;
 }
 
-interface Article {
-  title: string;
-  content: string;
-}
+let u: Partial<User>;
 
-function printUserProperty(user: User, prop: keyof User) {
-  console.log(user[prop]);
-}
-
-type Obj<O> = {
-  readonly [key in keyof O]: O[key];
+let u2: Required<User> = {
+  age: 1,
+  name: '11',
 };
 
-const u: Obj<User> = {
-  loginId: 'aa',
-  loginWd: 'b',
-  age: 11,
+let u3: Readonly<User> = {
+  age: 1,
+  name: '11',
 };
-const a: Obj<Article> = {
-  title: '',
-  content: '',
+
+let u4: Exclude<'a' | 'b' | 'c', 'c'> = 'a';
+let u44: Extract<'a' | 'b' | 'c', 'c'> = 'c';
+let u5: Omit<{ a: string; b: string; c: string }, 'c'> = { a: 'a', b: 'b' };
+let u55: Pick<{ a: string; b: string; c: string }, 'c'> = { c: 'c' };
+
+type str = string | null | undefined;
+
+type strNotEmpty = NonNullable<str>;
+
+type Func = () => string;
+
+type num = ReturnType<Func>;
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+let a: ReturnType<typeof sum>;
+
+class User {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const user: InstanceType<typeof User> = {
+  age: 1,
+  name: '',
 };
